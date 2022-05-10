e2c4ea4fb29f9a9940b7fc1c2cdc8ef1e8c77707
Thomas FERREIRA <thomas.ferreira@kafein-studio.fr>
2022-05-10 20:50:19 +0200

    add event

--- test_01.py
+++ test_01.py
@@ -9,6 +9,7 @@ load_dotenv()
 ##DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 DISCORD_TOKEN = 'OTczNjEzMjA2ODA5NTA1Nzky.GnUKFf.QKV5J9NZhYieVwvDUG5zPyXHPhYEmQmiEcUiWE'
 bot = discord.Client()
+list_Sortie = []
 
 @bot.event
 async def on_ready():
@@ -18,9 +19,16 @@ async def on_ready():
 async def on_message(message):
     if message.author == bot.user:
         return
-    if message.content == 'hello':
+    if message.content == 'on ma':
         await message.channel.send(f'Hey {message.author}')
     if message.content == 'goodbye':
         await message.channel.send(f'Goodbye {message.author}')
+
+@bot.event
+async def on_message(msg):
+    if 'BK' in msg.content:
+        print('Keyword found')
+        await msg.channel.send(f'https://tenor.com/view/saber-eating-bread-ubw-fate-gif-19103530')
+    
 
 bot.run(DISCORD_TOKEN)
