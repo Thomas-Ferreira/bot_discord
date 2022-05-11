7bc41ece238dc4664b9a8228c93bf3997ac50d39
Thomas FERREIRA <thomas.ferreira@kafein-studio.fr>
2022-05-11 16:00:08 +0200

    use of api

---
+++ Api.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import requests
+from pprint import pprint
+import json
+
+class Api :
+
+    def data(self) :
+
+        res=requests.get('https://yande.re/post.json?limit=100&tags=genshin_impact')
+        li_url = []
+        json_item = json.loads(res.text)
+
+        for item in range(0,99):
+            url =  json_item[item]['file_url']
+            li_url.append(url)
+    
+        dictionary = {i : li_url[i] for i in range(0, len(li_url) ) }
+
+        return dictionary
+
+#data = Api()
+#dict = data.data()
+#pprint(dict[0])
--- test_01.py
+++ test_01.py
@@ -6,6 +6,8 @@ import os
 from dotenv import load_dotenv
 import Scrapper
 from Scrapper import ScrapperTest
+from Api import Api
+import random
 
 BOT_NAME = "Borgar_bot"
 
@@ -14,8 +16,8 @@ load_dotenv()
 DISCORD_TOKEN = 'OTczNjEzMjA2ODA5NTA1Nzky.GnUKFf.QKV5J9NZhYieVwvDUG5zPyXHPhYEmQmiEcUiWE'
 bot = discord.Client()
 
-scrapper1 = ScrapperTest()
-dict = scrapper1.navigate()
+#scrapper1 = ScrapperTest()
+#dict = scrapper1.navigate()
 
 bot_help_message = """
 :: Bot Usage ::
@@ -30,7 +32,6 @@ bot = commands.Bot(command_prefix="!")
 async def on_ready():
     print(f'{bot.user} has logged in.')
 
-
 @bot.event
 async def on_message(msg):
     if msg.author == bot.user:
@@ -40,14 +41,21 @@ async def on_message(msg):
     if msg.content == 'goodbye':
         await msg.channel.send(f'Goodbye {msg.author}')
 
-
     if msg.content == '!mc':
+        #scrapper1 = ScrapperTest()
+        #dict = scrapper1.navigate()
         await msg.channel.send(dict)
 
 
     if 'BK' in msg.content:
         print('Keyword found')
         await msg.channel.send(f'https://tenor.com/view/saber-eating-bread-ubw-fate-gif-19103530')
+
+    if msg.content == 'waifu please':
+        data = Api()
+        dict = data.data()
+        nbrAléatoire = random.randint(0,98)
+        await msg.channel.send(dict[nbrAléatoire])
 
 @bot.command()
 async def mc(ctx, arg):
